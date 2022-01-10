edb1ade10d6d6a0f4f4bd4db0f8b7c88e4bff8ed
cledson-leite <csbetsonline@gmail.com>
2022-01-10 13:31:04 -0300

    feat: ensure it is deleted when requested

--- lib/home/services/dio_client.dart
+++ lib/home/services/dio_client.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:productcase/home/data/datasources/i_client.dart';
+import 'package:productcase/home/data/datasources/i_delete_client.dart';
 import 'package:productcase/home/data/models/product_model.dart';
 import 'package:productcase/home/data/repository/error/list_product_exception.dart';
 
-class DioClient implements IClient {
+class DioClient implements IClient, IDeleteClient {
   final Dio service;
 
   DioClient(this.service) {
@@ -21,6 +22,17 @@ class DioClient implements IClient {
         models.add(ProductModel.fromMap(value, key));
       });
       return models;
+    } catch (_) {
+      throw UnexpectedException();
+    }
+  }
+
+  @override
+  Future<bool> delete(String id) async {
+    try {
+      final result = await service.delete('/products/$id.json');
+      if (result.data == null) return false;
+      return result.data;
     } catch (_) {
       throw UnexpectedException();
     }
